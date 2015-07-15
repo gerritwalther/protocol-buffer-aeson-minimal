570d7e22127aee3ce442217135ccf8b5a9bcdfdf
Gerrit Walther <gerritwalther@gmail.com>
2015-07-15 17:51:55 +0200

    Added instances for using ToJSON and FromJSON for the Registrant message

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,12 @@ import              Text.ProtocolBuffers.Basic
 
 import              Registrant.Registrant
 import              JSONModel
+
+import Data.Aeson ((.=))
+import qualified Data.Aeson as J
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Lazy.Char8 as BCL
+import qualified Data.Text.Encoding as T
 -------------------------------------------------------------------------------
 
 -- jsonOptions parameter für deriveJSON muss importiert sein, daher definiert in anderem modul
@@ -16,14 +22,21 @@ $(deriveJSON jsonOptions ''Type)
 
 -------------------------------------------------------------------------------
 
+instance ToJSON Registrant where
+    toJSON registrant =
+        J.object ["name" .= name registrant]
+
+instance ToJSON Utf8 where
+    toJSON u = J.String (T.decodeUtf8 (BL.toStrict  (utf8 u)))
+
 main :: IO ()
 main = do
     let registrant = Registrant { name = Utf8 "haskell" }
-        register = Response { _data = "{" ++ messagePutText registrant ++ "}"
+        register = Response { _data = BCL.unpack (J.encode registrant)
                             , _profile = Nothing
                             , _request = "register"
                             , _type = Type { _name = "normal" }
                             }
         registerJSON = encode register
 
-    putStrLn $ show registerJSON
+    putStrLn $ BCL.unpack registerJSON
